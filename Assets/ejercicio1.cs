354bac5cd3fe88531124bff080d146917349409f
valentinocornejo <101400293+valentinocornejo@users.noreply.github.com>
2022-04-12 13:56:47 -0300

    Ejercicio Terminado

--- Assets/ejercicio1.cs
+++ Assets/ejercicio1.cs
@@ -22,22 +22,41 @@ public class ejercicio1 : MonoBehaviour
     void Start()
     {
         int estadiolleno = 36600;
-        int espectadorestotales;
-        int campo = 20400;
-        int platea = 16200;
+        int espectadorestotales = personascampo + personasplatea;
+        //int campo = 20400;
+        //int platea = 16200;
         int preciocampo = 1200;
         int precioplatea = 2000;
         int recaudadocampo;
         int recaudadoplatea;
         int totalrecaudado;
+        int restovacante;
+
+        if(personascampo <= 0 | personasplatea <= 0 | personascampo > 20400 | personasplatea > 16200)
+        {
+            Debug.Log("Ingrese los valores reales");
+        }
 
         recaudadocampo = personascampo * preciocampo;
         recaudadoplatea = personasplatea * precioplatea;
         totalrecaudado = recaudadocampo + recaudadoplatea;
         Debug.Log("Se recaudo" + recaudadocampo + "por las personas en el campo y "+ recaudadoplatea + " por las personas en la platea. En total se recaudo" + totalrecaudado);
 
-    }
+         restovacante = estadiolleno - personascampo - personasplatea;
+        if(restovacante == estadiolleno)
+        {
+            Debug.Log("Sold Out");
+        }
+        else if(restovacante >= espectadorestotales / 2)
+        {
+            Debug.Log("Fue un Exíto");
+        }
+        else
+        {
+            Debug.Log("Debemos mejorar la convocatoria");
+        }
 
+    }
     // Update is called once per frame
     void Update()
     {
